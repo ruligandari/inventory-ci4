16b3aba24b90c073d50039c8ed30d5d36f7017da
ruligandari <ruligandari@gmail.com>
2023-05-13 12:13:15 +0700

    menambahkan database

--- cisnu-petshop (1).sql
+++ cisnu-petshop (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 29 Apr 2023 pada 11.41
+-- Waktu pembuatan: 13 Bulan Mei 2023 pada 07.11
 -- Versi server: 10.4.27-MariaDB
--- Versi PHP: 8.2.0
+-- Versi PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -59,7 +59,7 @@ INSERT INTO `barang` (`id_barang`, `nama_barang`, `id_supplier`, `harga`) VALUES
 --
 
 CREATE TABLE `barang_keluar` (
-  `id_barang_keluar` varchar(255) NOT NULL,
+  `id_barang_keluar` int(11) NOT NULL,
   `id_supplier` varchar(255) NOT NULL,
   `id_barang` varchar(255) NOT NULL,
   `tanggal_pesan` date NOT NULL,
@@ -73,43 +73,72 @@ CREATE TABLE `barang_keluar` (
 --
 
 INSERT INTO `barang_keluar` (`id_barang_keluar`, `id_supplier`, `id_barang`, `tanggal_pesan`, `harga`, `jumlah`, `status`) VALUES
-('1', 'SUP0001', 'BR-0001', '2023-02-15', 250000, 3, 'Diterima'),
-('10', 'SUP0001', 'BR-0010', '2023-02-23', 3000, 7, 'Diterima'),
-('11', 'SUP0001', 'BR-0011', '2023-02-24', 24000, 15, 'Diterima'),
-('12', 'SUP0001', 'BR-0012', '2023-02-25', 1500, 9, 'Diterima'),
-('13', 'SUP0001', 'BR-0001', '2023-03-15', 250000, 10, 'Diterima'),
-('14', 'SUP0001', 'BR-0002', '2023-03-16', 140000, 4, 'Diterima'),
-('15', 'SUP0001', 'BR-0003', '2023-03-17', 160000, 7, 'Diterima'),
-('16', 'SUP0001', 'BR-0004', '2023-03-18', 165000, 9, 'Diterima'),
-('17', 'SUP0001', 'BR-0005', '2023-03-19', 29000, 8, 'Diterima'),
-('18', 'SUP0001', 'BR-0006', '2023-03-20', 69000, 6, 'Diterima'),
-('19', 'SUP0001', 'BR-0007', '2023-03-21', 21000, 18, 'Diterima'),
-('2', 'SUP0001', 'BR-0002', '2023-02-15', 140000, 2, 'Diterima'),
-('20', 'SUP0001', 'BR-0008', '2023-03-22', 8000, 9, 'Diterima'),
-('21', 'SUP0001', 'BR-0009', '2023-03-23', 3500, 8, 'Diterima'),
-('22', 'SUP0001', 'BR-0010', '2023-03-24', 3000, 10, 'Diterima'),
-('23', 'SUP0001', 'BR-0011', '2023-03-25', 24000, 3, 'Diterima'),
-('24', 'SUP0001', 'BR-0012', '2023-03-26', 1500, 6, 'Diterima'),
-('25', 'SUP0001', 'BR-0001', '2023-04-15', 250000, 10, 'Diterima'),
-('26', 'SUP0001', 'BR-0002', '2023-04-16', 140000, 4, 'Diterima'),
-('27', 'SUP0001', 'BR-0003', '2023-04-17', 160000, 7, 'Diterima'),
-('28', 'SUP0001', 'BR-0004', '2023-04-18', 165000, 9, 'Diterima'),
-('29', 'SUP0001', 'BR-0005', '2023-04-19', 29000, 8, 'Diterima'),
-('3', 'SUP0001', 'BR-0003', '2023-02-16', 160000, 3, 'Diterima'),
-('30', 'SUP0001', 'BR-0006', '2023-04-20', 69000, 6, 'Diterima'),
-('31', 'SUP0001', 'BR-0007', '2023-04-21', 21000, 18, 'Diterima'),
-('32', 'SUP0001', 'BR-0008', '2023-04-22', 8000, 9, 'Diterima'),
-('33', 'SUP0001', 'BR-0009', '2023-04-23', 3500, 8, 'Diterima'),
-('34', 'SUP0001', 'BR-0010', '2023-04-24', 3000, 10, 'Diterima'),
-('35', 'SUP0001', 'BR-0011', '2023-04-25', 24000, 3, 'Diterima'),
-('36', 'SUP0001', 'BR-0012', '2023-04-26', 1500, 6, 'Diterima'),
-('4', 'SUP0001', 'BR-0004', '2023-02-17', 165000, 3, 'Diterima'),
-('5', 'SUP0001', 'BR-0005', '2023-02-18', 29000, 14, 'Diterima'),
-('6', 'SUP0001', 'BR-0006', '2023-02-19', 69000, 9, 'Diterima'),
-('7', 'SUP0001', 'BR-0007', '2023-02-20', 21000, 15, 'Diterima'),
-('8', 'SUP0001', 'BR-0008', '2023-02-21', 8000, 10, 'Diterima'),
-('9', 'SUP0001', 'BR-0009', '2023-02-22', 3500, 9, 'Diterima'),
-('id_barang_keluar', 'id_supplier', 'Id_barang', '0000-00-00', 0, 0, 'status');
+(1, 'SUP0001', 'BR-0001', '2023-02-15', 250000, 3, 'Keluar'),
+(2, 'SUP0001', 'BR-0002', '2023-02-15', 140000, 2, 'Keluar'),
+(3, 'SUP0001', 'BR-0003', '2023-02-16', 160000, 3, 'Keluar'),
+(4, 'SUP0001', 'BR-0004', '2023-02-17', 165000, 3, 'Keluar'),
+(5, 'SUP0001', 'BR-0005', '2023-02-18', 29000, 14, 'Keluar'),
+(6, 'SUP0001', 'BR-0006', '2023-02-19', 69000, 9, 'Keluar'),
+(7, 'SUP0001', 'BR-0007', '2023-02-20', 21000, 15, 'Keluar'),
+(8, 'SUP0001', 'BR-0008', '2023-02-21', 8000, 10, 'Keluar'),
+(9, 'SUP0001', 'BR-0009', '2023-02-22', 3500, 9, 'Keluar'),
+(10, 'SUP0001', 'BR-0010', '2023-02-23', 3000, 7, 'Keluar'),
+(11, 'SUP0001', 'BR-0011', '2023-02-24', 24000, 15, 'Keluar'),
+(12, 'SUP0001', 'BR-0012', '2023-02-25', 1500, 9, 'Keluar'),
+(13, 'SUP0001', 'BR-0001', '2023-03-15', 250000, 10, 'Keluar'),
+(14, 'SUP0001', 'BR-0002', '2023-03-16', 140000, 4, 'Keluar'),
+(15, 'SUP0001', 'BR-0003', '2023-03-17', 160000, 7, 'Keluar'),
+(16, 'SUP0001', 'BR-0004', '2023-03-18', 165000, 9, 'Keluar'),
+(17, 'SUP0001', 'BR-0005', '2023-03-19', 29000, 8, 'Keluar'),
+(18, 'SUP0001', 'BR-0006', '2023-03-20', 69000, 6, 'Keluar'),
+(19, 'SUP0001', 'BR-0007', '2023-03-21', 21000, 18, 'Keluar'),
+(20, 'SUP0001', 'BR-0008', '2023-03-22', 8000, 9, 'Keluar'),
+(21, 'SUP0001', 'BR-0009', '2023-03-23', 3500, 8, 'Keluar'),
+(22, 'SUP0001', 'BR-0010', '2023-03-24', 3000, 10, 'Keluar'),
+(23, 'SUP0001', 'BR-0011', '2023-03-25', 24000, 3, 'Keluar'),
+(24, 'SUP0001', 'BR-0012', '2023-03-26', 1500, 6, 'Keluar'),
+(25, 'SUP0001', 'BR-0001', '2023-04-15', 250000, 10, 'Keluar'),
+(26, 'SUP0001', 'BR-0002', '2023-04-16', 140000, 4, 'Keluar'),
+(27, 'SUP0001', 'BR-0003', '2023-04-17', 160000, 7, 'Keluar'),
+(28, 'SUP0001', 'BR-0004', '2023-04-18', 165000, 9, 'Keluar'),
+(29, 'SUP0001', 'BR-0005', '2023-04-19', 29000, 8, 'Keluar'),
+(30, 'SUP0001', 'BR-0006', '2023-04-20', 69000, 6, 'Keluar'),
+(31, 'SUP0001', 'BR-0007', '2023-04-21', 21000, 18, 'Keluar'),
+(32, 'SUP0001', 'BR-0008', '2023-04-22', 8000, 9, 'Keluar'),
+(33, 'SUP0001', 'BR-0009', '2023-04-23', 3500, 8, 'Keluar'),
+(34, 'SUP0001', 'BR-0010', '2023-04-24', 3000, 10, 'Keluar'),
+(35, 'SUP0001', 'BR-0011', '2023-04-25', 24000, 3, 'Keluar'),
+(36, 'SUP0001', 'BR-0012', '2023-04-26', 1500, 6, 'Keluar'),
+(85, 'SUP0001', 'BR-0001', '2023-05-01', 250000, 8, 'Keluar'),
+(86, 'SUP0001', 'BR-0002', '2023-05-01', 140000, 4, 'Keluar'),
+(87, 'SUP0001', 'BR-0002', '2023-05-01', 140000, 4, 'Keluar');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `id_barang_masuk` int(11) NOT NULL,
+  `id_supplier` varchar(255) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
+  `tanggal_pesan` date NOT NULL,
+  `harga` double NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`id_barang_masuk`, `id_supplier`, `id_barang`, `tanggal_pesan`, `harga`, `jumlah`, `status`) VALUES
+(1, 'SUP0001', 'BR-0001', '2023-05-01', 250000, 8, 'Dikeluarkan'),
+(2, 'SUP0001', 'BR-0002', '2023-05-01', 140000, 4, 'Dikeluarkan'),
+(3, 'SUP0001', 'BR-0002', '2023-05-01', 140000, 4, 'Dikeluarkan'),
+(4, 'SUP0001', 'BR-0002', '2023-05-01', 140000, 4, 'Masuk'),
+(5, 'SUP0001', 'BR-0005', '2023-05-02', 29000, 10, 'Masuk');
 
 -- --------------------------------------------------------
 
@@ -120,7 +149,7 @@ INSERT INTO `barang_keluar` (`id_barang_keluar`, `id_supplier`, `id_barang`, `ta
 CREATE TABLE `barang_pesanan` (
   `id_barang_pesanan` int(11) UNSIGNED NOT NULL,
   `id_supplier` varchar(11) NOT NULL,
-  `nama_barang` varchar(255) NOT NULL,
+  `id_barang` varchar(255) NOT NULL,
   `tanggal_pesan` date NOT NULL,
   `jumlah` int(11) NOT NULL,
   `status` enum('Dipesan','Dikirim','Diterima') NOT NULL DEFAULT 'Dipesan'
@@ -130,18 +159,9 @@ CREATE TABLE `barang_pesanan` (
 -- Dumping data untuk tabel `barang_pesanan`
 --
 
-INSERT INTO `barang_pesanan` (`id_barang_pesanan`, `id_supplier`, `nama_barang`, `tanggal_pesan`, `jumlah`, `status`) VALUES
-(1, 'SUP0001', 'Bolt 100 Kg', '2023-03-22', 2, 'Dikirim'),
-(2, 'SUP0002', 'Bolt 50 Kg', '2023-03-22', 3, 'Dikirim'),
-(3, 'SUP0001  ', 'Ikan Hias Mas', '0000-00-00', 10, 'Dikirim'),
-(4, 'SUP0001  ', 'Ikan Koi', '2023-03-23', 12, 'Dikirim'),
-(5, 'SUP0002', 'Ikan Sapu-SApu', '2023-03-23', 8, 'Dikirim'),
-(6, 'SUP0001  ', 'Wiskas 250gram', '2023-03-23', 12, 'Dikirim'),
-(7, 'SUP0001  ', 'Aquaruin Ukuran 200x20x30', '2023-03-23', 5, 'Dikirim'),
-(8, 'SUP0001  ', 'Batu Malang 10kg', '2023-03-23', 9, 'Dikirim'),
-(9, 'SUP0001  ', 'Pasir Gumpal Wangi Zeolit 25kg', '2023-04-29', 6, 'Dipesan'),
-(10, 'SUP0001  ', 'Wishkas Adult Tuna dan Ocean Fish 80gr (box)', '2023-04-29', 4, 'Dipesan'),
-(11, 'SUP0001  ', 'Wishkas Dry Junior  450gr', '2023-04-29', 8, 'Dipesan');
+INSERT INTO `barang_pesanan` (`id_barang_pesanan`, `id_supplier`, `id_barang`, `tanggal_pesan`, `jumlah`, `status`) VALUES
+(3, 'SUP0001  ', 'BR-0002', '2023-05-01', 4, 'Diterima'),
+(4, 'SUP0001  ', 'BR-0005', '2023-05-02', 10, 'Diterima');
 
 -- --------------------------------------------------------
 
@@ -210,7 +230,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama`, `email`, `password`, `role`) VALUES
-(1, 'Pimpinan', 'pimpinan@gmail.com', '$2y$10$W.k3R/Ou/PxuPzDid9wmiO9Dg2gNIk3eA1nZL8CVd4bEyKYSLWKiC', '1'),
+(1, 'Pimpinan', 'pimpinan@gmail.com', '$2y$10$zEkY1yb9KfJGyigYisNDG.ZHI8jrgSxgexfY4r9bKPpNmuLDNzqvK', '1'),
 (2, 'Pegawai', 'pegawai@gmail.com', '$2y$10$v.vQ1QXn.Ra2JmdMDyJF.uPcDZMaDGV1rMTbHKJnN7HbEjHRCbX1m', '2'),
 (3, 'Supplier', 'supplier@gmail.com', '$2y$10$RNURa3AfkQ.74bY79G3c2.8vhZ6PDPQH1l3PhCTgCLgArnXYIaxUC', '3'),
 (6, 'Pegawai 2', 'pegawai2@gmail.com', '$2y$10$U7RKcXZ178V0eiYodyOQtOeP7m4KNYCIIO.OasqLSyDdPq4J9OOo6', '2');
@@ -220,18 +240,16 @@ INSERT INTO `users` (`id`, `nama`, `email`, `password`, `role`) VALUES
 --
 
 --
--- Indeks untuk tabel `barang`
---
-ALTER TABLE `barang`
-  ADD PRIMARY KEY (`id_barang`);
-
---
 -- Indeks untuk tabel `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  ADD PRIMARY KEY (`id_barang_keluar`),
-  ADD KEY `id_supplier` (`id_supplier`),
-  ADD KEY `id_barang` (`id_barang`);
+  ADD PRIMARY KEY (`id_barang_keluar`);
+
+--
+-- Indeks untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`id_barang_masuk`);
 
 --
 -- Indeks untuk tabel `barang_pesanan`
@@ -262,10 +280,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  MODIFY `id_barang_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `id_barang_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT untuk tabel `barang_pesanan`
 --
 ALTER TABLE `barang_pesanan`
-  MODIFY `id_barang_pesanan` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_barang_pesanan` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `migrations`
